f2f12f2bd8c53c3ec42185b8940e42b0d1393840
Roman Jaquez <romanjaquez@Romans-MacBook-Pro-3.local>
2020-09-19 11:50:52 -0400

    add link to save date

--- src/app/components/hero-banner/hero-banner.component.ts
+++ src/app/components/hero-banner/hero-banner.component.ts
@@ -13,4 +13,14 @@ export class HeroBannerComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSaveDateAction() {
+    // TODO: let's make it configurable instead.
+    let startDate = '20201031T140000Z';
+    let endDate = '20201031T190000Z';
+    let title = 'GDG NorthEast DevFest 2020';
+    let location = 'Online Event - Stay Tuned for DevFest Link'
+    let details = 'GDG NorthEast DevFest 2020 is here!. Join us for community-led, virtual sessions from Google Developers products and Communities leaders. Watch the keynote, learn from technical talks, and meet fellow local developers online.';
+
+    window.open(`http://www.google.com/calendar/event?action=TEMPLATE&dates=${startDate}/${endDate}&text=${title}&details=${details}&location=${location}`)
+  }
 }
